7c3544e755c47d35a5bf09ce231283a5f4fe5dce
Luiz Gustavo Díaz <contatoluisgustavodias@gmail.com>
2024-09-06 10:17:46 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa")
+    console.log(section);
+
+    let campoPesquisa = document.getElementById ("campo-pesquisa").value
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nenhum resultado encontrado! Digite sua busca</p>"
+     return
+    }
+
+campoPesquisa = campoPesquisa.toLowerCase()
+
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+    
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) ||tags.includes(campoPesquisa)   ) {
+        resultados += `
+        <div class="item-resultado">
+            <h2>
+            <a href="#" target="_blank">${dado.titulo}</h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href=${dado.link}  target="_blank">Mais informações</a>
+            
+        </div>
+    `;
+      }
+    }
+    
+
+    if (!resultados) {resultados = "<p>Nada foi encontrado<p/>"}
+    section.innerHTML = resultados
+
+}
+
---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [ 
+    {
+        titulo: "Deipnon: Uma Refeição Sagrada na Grécia Antiga",
+        descricao: "o banquete dos Deuses Gregos",
+        link: "http://paganismohelenico.blogspot.com/2016/07/deipnon-de-hecate-refeicao-de-hecate.html",
+        tags: "Hecate Hekate",
+        
+        
+        },
+
+        {
+            titulo: "Noumenia: O Novo Mês na Grécia Antiga",
+            descricao: "O primeiro dia do mês lunar",
+            link: "https://jardimdehekate.wordpress.com/2023/08/17/explorando-a-energia-sagrada-da-noumenia-com-hekate/",
+            tags: ""
+             },
+    
+];
+
+
+    
